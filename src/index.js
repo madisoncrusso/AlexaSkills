6385bcdbeeebc4d08ffa59e687edc5fbb4390b1e
Madison Russo <madisoncrusso@gmail.com>
2016-07-23 11:21:15 -0400

    Publish

---
+++ src/index.js
@@ -0,0 +1,125 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+/**
+ * This simple sample has no external dependencies or session management, and shows the most basic
+ * example of how to create a Lambda function for handling Alexa Skill requests.
+ *
+ * Examples:
+ * One-shot model:
+ *  User: "Alexa, ask Space Geek for a space fact"
+ *  Alexa: "Here's your space fact: ..."
+ */
+
+/**
+ * App ID for the skill
+ */
+var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+
+/**
+ * Array containing space facts.
+ */
+var FACTS = [
+    "Before he renamed himself Bob Dylan, Robert Allen Zimmerman briefly went by Elston Gunn.",
+    "Dylan’s father, Abe, was a semi-professional baseball player before he contracted polio in his early twenties.",
+    "According to Bob Spitz, author of The Beatles: The Biography, it was Dylan who first introduced the Fab Four to marijuana.",
+    "Dylan’s first professional recording was as a harmonica player at a Harry Belafonte session.",
+    "His great-grandfather and uncles owned the biggest movie theaters in Hibbing, Minnesota, allowing a young Dylan to watch films for free.",
+    "After seeing Rebel Without a Cause, Dylan became obsessed with James Dean.",
+    "In 10th grade, Dylan and his back-up band were rejected from a high school talent show because the student council judged his performance at the audition too shocking.",
+    "Before he flunked out of the University of Minnesota, Dylan pledged to the fraternity Sigma Alpha Mu.",
+    "In college, Dylan was known for scamming his friends out of cigarettes and articles of clothing.",
+    "In 1960, Tommy Smothers of the Smothers Brothers tried to get the manager of a Denver club to kick Dylan off the bill, complaining that his voice was awful.",
+    "During his days in Greenwich Village in the 1960s, Dylan became a chess fiend.",
+    "In 2004, Dylan earned an honorary doctorate in music from the University of St. Andrews.",
+    "The first time Donovan and Bob Dylan met, Dylan and his entourage all wore Halloween masks."
+    "Dylan didn't show up to his high school graduation."
+    "Dylan used to go by the name of Elston Gunn."
+    "Charlie Chaplin is one of Dylan's biggest influences."
+];
+
+/**
+ * The AlexaSkill prototype and helper functions
+ */
+var AlexaSkill = require('./AlexaSkill');
+
+/**
+ * SpaceGeek is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var Fact = function () {
+    AlexaSkill.call(this, APP_ID);
+};
+
+// Extend AlexaSkill
+Fact.prototype = Object.create(AlexaSkill.prototype);
+Fact.prototype.constructor = Fact;
+
+Fact.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    //console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
+    // any initialization logic goes here
+};
+
+Fact.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    //console.log("onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    handleNewFactRequest(response);
+};
+
+/**
+ * Overridden to show that a subclass can override this function to teardown session state.
+ */
+Fact.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    //console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId + ", sessionId: " + session.sessionId);
+    // any cleanup logic goes here
+};
+
+Fact.prototype.intentHandlers = {
+    "GetNewFactIntent": function (intent, session, response) {
+        handleNewFactRequest(response);
+    },
+
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        response.ask("You can say tell me a space fact, or, you can say exit... What can I help you with?", "What can I help you with?");
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    }
+};
+
+/**
+ * Gets a random new fact from the list and returns to the user.
+ */
+function handleNewFactRequest(response) {
+    // Get a random space fact from the space facts list
+    var factIndex = Math.floor(Math.random() * FACTS.length);
+    var randomFact = FACTS[factIndex];
+
+    // Create speech output
+    var speechOutput = "Here's your fact: " + randomFact;
+    var cardTitle = "Your Fact";
+    response.tellWithCard(speechOutput, cardTitle, speechOutput);
+}
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    // Create an instance of the SpaceGeek skill.
+    var fact = new Fact();
+    fact.execute(event, context);
+};
+
